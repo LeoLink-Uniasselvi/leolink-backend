6fb806ae261b57e6b279a1d61c655c6a9e30c6b5
Arthur Santos <arthurvilmar.santos@gmail.com>
2025-09-16 19:56:50 -0300

    feat: ✨ adicionar classe BaseEntity com campos de timestamp

---
+++ src/entities/base.entity.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import newDateUtc from '@/utils/new-date-utc';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('timestampz')
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column('timestampz')
+  @UpdateDateColumn({ name: 'updated_at', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column('timestampz')
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
+  deletedAt: Date | null;
+
+  constructor() {
+    this.createdAt = newDateUtc();
+    this.createdAt = newDateUtc();
+  }
+}
