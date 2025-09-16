84963ebd566f2b2838ab0d782e85f31506cd6bb2
Arthur Santos <arthurvilmar.santos@gmail.com>
2025-09-16 20:03:31 -0300

    feat: ✨ adicionar classe BaseResponseDto com campos de resposta padrão

---
+++ src/dtos/base-response.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class BaseResponseDto {
+  @ApiProperty({ format: 'uuid', readOnly: true })
+  id!: string;
+
+  @ApiProperty({ type: String, format: 'date-time', readOnly: true })
+  createdAt!: Date;
+
+  @ApiProperty({ type: String, format: 'date-time', readOnly: true })
+  updatedAt!: Date;
+  
+  @ApiProperty({ type: String, format: 'date-time', readOnly: true })
+  deletedAt!: Date | null;
+}
