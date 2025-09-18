0975567e46c1d5650c089b66a2ead383cd202957
Arthur Santos <arthurvilmar.santos@gmail.com>
2025-09-18 10:15:49 -0300

    feat: ✨ corrigir inicialização do ConfigModule e adicionar tratamento de erro na inicialização da aplicação

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  ConfigModule.forRoot();
+  await ConfigModule.forRoot();
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('LeoLink')
-    .setDescription('Uma rede social voltada para o mundo acadêmico. Desenvolvida pelos alunos da Uniasselvi. Turmas: EGS0011, EGS0016 e EGS0016.')
+    .setDescription(
+      'Uma rede social voltada para o mundo acadêmico. Desenvolvida pelos alunos da Uniasselvi. Turmas: EGS0011, EGS0016 e EGS0016.',
+    )
     .setVersion('0.1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
@@ -20,4 +22,8 @@ async function bootstrap() {
 
   await app.listen(process.env.APP_PORT || 3000);
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Erro ao inicializar a aplicação:', error);
+  process.exit(1);
+});
